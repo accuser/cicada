c264dfcf578f3ea3fc58ac1d610a1d93a7f1802d
Matthew Gibbons <mhgibbons@me.com>
2011-04-08 17:35:59 +0100

    Need prime before we use it ;)

--- cicada_app.rb
+++ cicada_app.rb
@@ -15,12 +15,12 @@ class CicadaApp < Sinatra::Base
   PRIMES = [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 ].freeze
 
   get '/:id.png' do
-    png = ChunkyPNG::Image.new(prime, 1, ChunkyPNG::Color::TRANSPARENT)
-
     srand(params[:id].to_i)
       
     prime = PRIMES[rand(PRIMES.size)]  
     color = ChunkyPNG::Color.rgba(rand(ChunkyPNG::Color::MAX), rand(ChunkyPNG::Color::MAX), rand(ChunkyPNG::Color::MAX), 128)
+
+    png = ChunkyPNG::Image.new(prime, 1, ChunkyPNG::Color::TRANSPARENT)
     
     for x in 0..(prime / 2)
       png[x, 0] = color
